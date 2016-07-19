5f65b4359d1168c0d4761b78225e936c28c24682
Julio Gori Corradi <jgcorradi@gmail.com>
2016-07-19 14:00:00 -0300

    Improving pascal case specs

--- spec/pascalcaseSpec.js
+++ spec/pascalcaseSpec.js
@@ -14,24 +14,36 @@ describe('String.prototype.toPascalCase Spec', () => {
             expect('blablabla'.toPascalCase()).toEqual('Blablabla');
         });
 
-        it('When it\'s a word spaced string', () => {
+        it('When it\'s a word spaced', () => {
             expect('bla bla bla'.toPascalCase()).toEqual('BlaBlaBla');
         });
 
-        it('When it\'s a word spaced string', () => {
-            expect('bla bla bla'.toPascalCase()).toEqual('BlaBlaBla');
+        it('When it\'s a word spaced that ends with a number', () => {
+            expect('bla bla bla 1'.toPascalCase()).toEqual('BlaBlaBla1');
         });
 
         it('When it\'s a word spaced by dashes', () => {
             expect('bla-bla-bla'.toPascalCase()).toEqual('BlaBlaBla');
         });
 
+        it('When it\'s a word spaced by dashes that ends with a number', () => {
+            expect('bla-bla-bla-1'.toPascalCase()).toEqual('BlaBlaBla1');
+        });
+
         it('When it\'s a word spaced by dots', () => {
             expect('bla.bla.bla'.toPascalCase()).toEqual('BlaBlaBla');
         });
 
+        it('When it\'s a word spaced by dots that ends with a number', () => {
+            expect('bla.bla.bla.1'.toPascalCase()).toEqual('BlaBlaBla1');
+        });
+
         it('When it\'s a word spaced by underscores', () => {
             expect('bla_bla_bla'.toPascalCase()).toEqual('BlaBlaBla');
+        });
+
+        it('When it\'s a word spaced by underscores that ends with a number', () => {
+            expect('bla_bla_bla_1'.toPascalCase()).toEqual('BlaBlaBla1');
         });
     });
 });
--- spec/reconfNodejsSpec.js
+++ spec/reconfNodejsSpec.js
@@ -6,15 +6,15 @@ let reconf = require('../lib/reconf-nodejs');
 describe('reconf-nodejs Spec', function () {
     beforeEach(function () {
         nock('http://config-reader.reconf.url')
-            .get('/product-name/product-component/db.pass?instance=server.name')
+            .get('/product-name/product-component/prop.name.1?instance=server.name')
             .reply(200, 'prop1');
 
         nock('http://config-reader.reconf.url')
-            .get('/product-name/product-component/db.url?instance=server.name')
+            .get('/product-name/product-component/prop.name.2?instance=server.name')
             .reply(200, 'prop2');
 
         nock('http://config-reader.reconf.url')
-            .get('/product-name/product-component/db.user?instance=server.name')
+            .get('/product-name/product-component/prop.name.3?instance=server.name')
             .reply(200, 'prop3');
 
         reconf = reconf({
@@ -23,26 +23,26 @@ describe('reconf-nodejs Spec', function () {
             , product    : 'product-name'
             , component  : 'product-component'
             , properties : [
-                  'db.pass'
-                , 'db.url'
-                , 'db.user'
+                  'prop.name.1'
+                , 'prop.name.2'
+                , 'prop.name.3'
             ]
             , instance   : 'server.name'
         });
     });
 
     afterAll(function () {
-        delete reconf.constructor.prototype.getDbPass;
-        delete reconf.constructor.prototype.getDbUrl;
-        delete reconf.constructor.prototype.getDbUser;
+        delete reconf.constructor.prototype.getPropName1;
+        delete reconf.constructor.prototype.getPropName2;
+        delete reconf.constructor.prototype.getPropName3;
     });
 
     describe('Should add properties methods', function () {
         it('When reconf is initiated with a proper settings object', function (done) {
             Promise.all([
-                  reconf.getDbPass()
-                , reconf.getDbUrl()
-                , reconf.getDbUser()
+                  reconf.getPropName1()
+                , reconf.getPropName2()
+                , reconf.getPropName3()
             ]).then(values => {
                 expect(values.length).toBe(3);
                 expect(values[0]).toBe('prop1');
